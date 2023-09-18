9396f581db8d7c931eed481264b7bf3fd7a0cec1
Gregor Betz <3662782+ggbetz@users.noreply.github.com>
2023-09-18 18:12:15 +0200

    new test

---
+++ tests/test_configs.py
@@ -0,0 +1,15 @@
+# test score function
+
+from logikon.schemas.configs import DebugConfig
+
+
+def test_configs():
+
+    config = DebugConfig()
+    assert config.expert_model == "text-ada-001"
+
+    config = DebugConfig(
+        expert_model="text-ada-002",
+        llm_framework="VLLM",
+    )
+    assert config.expert_model == "text-ada-002"
