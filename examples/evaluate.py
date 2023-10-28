de50642ba8435a1acdc69dc42247762e9c528316
Gregor Betz <3662782+ggbetz@users.noreply.github.com>
2023-10-28 17:35:10 +0200

    black

--- examples/evaluate.py
+++ examples/evaluate.py
@@ -22,24 +22,24 @@ import typer
 from logikon.schemas.configs import ScoreConfig
 
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(name)s - %(levelname)s - %(message)s'
-)
+logging.basicConfig(level=logging.INFO, format='%(name)s - %(levelname)s - %(message)s')
 logging.info(f"Installed `logikon` module version: {logikon.__version__}")
 
 
 @dataclasses.dataclass
 class ReferenceDataset:
     """Reference dataset metadata."""
+
     path: str
     id_field: str = None
     split: str = None
     revision: str = None
 
+
 @dataclasses.dataclass
 class EvalResultRecord:
     """Evaluation result record."""
+
     id: str
     reference_id: str
     reference_dataset: dict
@@ -61,12 +61,11 @@ def load_results(results_dataset: dict) -> list[EvalResultRecord]:
         repo_type="dataset",
         token=os.environ["HUGGINGFACEHUB_API_TOKEN"],
     ):
-            
         eval_file = hf_hub.hf_hub_download(
             results_dataset["path"],
             filename=results_dataset["filename"],
             subfolder=results_dataset["subfolder"],
-            repo_type="dataset"
+            repo_type="dataset",
         )
 
         with open(eval_file) as f:
@@ -78,7 +77,7 @@ def load_results(results_dataset: dict) -> list[EvalResultRecord]:
 
     else:
         logging.info("No eval results loaded.")
-        
+
     return eval_results
 
 
@@ -90,11 +89,11 @@ def upload_results(eval_results: list[EvalResultRecord], results_dataset: dict):
             tmpf.write(json.dumps(dataclasses.asdict(record)).encode("utf-8"))
             tmpf.write("\n".encode("utf-8"))
         tmpf.flush()
-        
+
         api = hf_hub.HfApi()
         api.upload_file(
             path_or_fileobj=tmpf.name,
-            path_in_repo=os.path.join(results_dataset["subfolder"],results_dataset["filename"]),
+            path_in_repo=os.path.join(results_dataset["subfolder"], results_dataset["filename"]),
             repo_id=results_dataset["path"],
             repo_type="dataset",
             token=os.environ["HUGGINGFACEHUB_API_TOKEN"],
@@ -103,10 +102,11 @@ def upload_results(eval_results: list[EvalResultRecord], results_dataset: dict):
         logging.info(f"Uploaded {len(eval_results)} eval results.")
 
 
-
 def main(
     artifacts: list[str] = typer.Option(["fuzzy_argmap_nx"], help="Artifacts to create."),
-    metrics: list[str] = typer.Option(["argmap_size", "argmap_avg_katz_centrality","argmap_attack_ratio"], help="Metrics to compute."),
+    metrics: list[str] = typer.Option(
+        ["argmap_size", "argmap_avg_katz_centrality", "argmap_attack_ratio"], help="Metrics to compute."
+    ),
     expert_model: str = typer.Option("stabilityai/StableBeluga-13B", help="Expert model to use."),
     expert_tokenizer: str = typer.Option("stabilityai/StableBeluga-13B", help="Expert tokenizer to use."),
     use_cuda: bool = typer.Option(True, help="Use cuda."),
@@ -121,7 +121,6 @@ def main(
     lang: str = typer.Option('en', help="Language."),
     upload_steps: int = typer.Option(5, help="Upload results every n steps."),
 ):
-    
     expert_model_kwargs = {}
     if use_cuda is not None:
         expert_model_kwargs["cuda"] = use_cuda
@@ -137,8 +136,10 @@ def main(
         metrics=metrics,
         global_kwargs=dict(
             expert_model=expert_model,
-            expert_model_kwargs = expert_model_kwargs,
-            generation_kwargs = dict(max_len=3072,),
+            expert_model_kwargs=expert_model_kwargs,
+            generation_kwargs=dict(
+                max_len=3072,
+            ),
             llm_framework=llm_framework,
         ),
     )
@@ -153,11 +154,13 @@ def main(
     results_dataset = dict(
         path=results_dataset_path,
         filename=f"{expert_model.replace('/','_')}_{reference_dataset.split}.jsonl",
-        subfolder=os.path.join("data",reference_dataset.path),
+        subfolder=os.path.join("data", reference_dataset.path),
     )
 
     if not os.environ.get("HUGGINGFACEHUB_API_TOKEN"):
-        logging.warning("No HUGGINGFACEHUB_API_TOKEN found in environment. Please set one to upload results to huggingface.co.")
+        logging.warning(
+            "No HUGGINGFACEHUB_API_TOKEN found in environment. Please set one to upload results to huggingface.co."
+        )
 
     # load the reference dataset
     ref_ds = datasets.load_dataset(
@@ -166,7 +169,9 @@ def main(
         revision=reference_dataset.revision,
     )
 
-    assert reference_dataset.id_field in ref_ds.features, f"Reference dataset must have {reference_dataset.id_field} field."
+    assert (
+        reference_dataset.id_field in ref_ds.features
+    ), f"Reference dataset must have {reference_dataset.id_field} field."
 
     # load the results dataset
     eval_results = load_results(results_dataset)
@@ -175,23 +180,18 @@ def main(
 
     # iterate over the reference dataset
     for enum, example in enumerate(tqdm(ref_ds)):
-
         if any(r.reference_id == example[reference_dataset.id_field] for r in eval_results):
             logging.debug(f"Skipping {example[reference_dataset.id_field]}: alreday scored.")
             continue
 
-        if len(example["history"].split())+len(example["text"].split()) > 900:
+        if len(example["history"].split()) + len(example["text"].split()) > 900:
             logging.warning(f"Too long: Skipping {example[reference_dataset.id_field]}")
             continue
 
         logging.debug(f"Scoring example {enum} of {len(ref_ds)}.\n")
         logging.debug(example["history"])
         logging.debug(example["text"])
-        debug_results = logikon.score(
-            prompt=example["history"],
-            completion=example["text"],
-            config=config
-        )
+        debug_results = logikon.score(prompt=example["history"], completion=example["text"], config=config)
 
         argmap = next((a.data for a in debug_results.artifacts if a.id == "fuzzy_argmap_nx"), None)
         argmap = nx.node_link_data(argmap) if argmap else {}
@@ -209,7 +209,7 @@ def main(
         scores["comments"] = scores.pop("comment")
 
         eval_record = EvalResultRecord(
-            id = str(uuid.uuid4()),
+            id=str(uuid.uuid4()),
             reference_id=example[reference_dataset.id_field],
             reference_dataset=dataclasses.asdict(reference_dataset),
             lang=example.get("lang", lang),
@@ -230,7 +230,6 @@ def main(
     # upload pending results
     if n_records_unsaved > 0:
         upload_results(eval_results, results_dataset)
-
 
 
 if __name__ == "__main__":
