777a5c0e3d01f4331deec8ffab3d41cc99fb2e1a
Gregor Betz <3662782+ggbetz@users.noreply.github.com>
2023-10-31 09:31:03 +0100

    temp .4

--- src/logikon/analysts/reconstruction/pros_cons_builder_lmql.py
+++ src/logikon/analysts/reconstruction/pros_cons_builder_lmql.py
@@ -246,7 +246,7 @@ def mine_reasons(prompt, completion, issue, prmpt_data: dict) -> List[Claim]:  #
 @lmql.query
 def build_pros_and_cons(reasons_data: list, issue: str, prmpt_data: dict):
     '''lmql
-    sample(temperature=.3, chunksize=6)
+    sample(temperature=.4, chunksize=6)
         reasons = [Claim(**reason_data) for reason_data in reasons_data]
         prmpt = PromptTemplate(**prmpt_data)
         """
