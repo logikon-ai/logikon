524ff2be3d3beba02f047933c5990dabaa6dfde1
Gregor Betz <3662782+ggbetz@users.noreply.github.com>
2023-11-24 14:05:56 +0100

    licence

--- src/logikon/__about__.py
+++ src/logikon/__about__.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2023-present Logikon AI
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: Apache-2.0
 __version__ = "0.0.1-dev1"
 __logo__ = """
 ██╗      ██████╗  ██████╗ ██╗██╗  ██╗ ██████╗ ███╗   ██╗     █████╗ ██╗
