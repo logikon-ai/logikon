9219f6836bf11e10c690b42381fce7386e582b17
Gregor Betz <3662782+ggbetz@users.noreply.github.com>
2023-11-29 07:25:17 +0100

    set v0.1.0

--- src/logikon/__about__.py
+++ src/logikon/__about__.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2023-present Logikon AI
 #
 # SPDX-License-Identifier: Apache-2.0
-__version__ = "0.0.1-dev1"
+__version__ = "0.1.0"
 __logo__ = """
 ██╗      ██████╗  ██████╗ ██╗██╗  ██╗ ██████╗ ███╗   ██╗     █████╗ ██╗
 ██║     ██╔═══██╗██╔════╝ ██║██║ ██╔╝██╔═══██╗████╗  ██║    ██╔══██╗██║
